21d0bbbf533457e6fdd694d9f9391ed99e011f9d
Christian Weichsel <c.weichsel@gmx.de>
2021-06-20 20:04:12 +0200

    save

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, Response
+from flask import Flask, render_template, request, redirect, send_file
 app = Flask(__name__)
 
 
@@ -8,19 +8,31 @@ def upload_image():
 
     return render_template("upload.html")
 
+def filename_allowed(filename : str) -> bool:
+    if filename == "":
+        return False
+    ext = filename.rsplit(".", 1)[1]
+    if ext.upper() != "TXT":
+        return False
+    return True
+
+
 @app.post("/upload_files")
 def upload_files():
     report = ""
+    
     for f in request.files.getlist("files[]"):
-        txt = f.read().decode("utf-8")
-        report += f.filename
-        report += txt
-        report += "\n"
+        if filename_allowed(f.filename):
+            txt = f.read().decode("utf-8")
+            report += f"{f.filename} : {txt} \n"
+        else:
+            report += f"{f.filename} nicht gültig. \n"
+        
+        with open("Output.txt", "w") as text_file:
+            text_file.write(report)
+ 
 
-    print(report)
-    response = Response(report, mimetype="text/csv")
-    response.headers['Content-Disposition'] = 'attachment; filename=fehlerreport.txt'
-    return response
+    return send_file("Output.txt",  as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
