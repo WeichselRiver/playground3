643db6de4f41eb7a2703b6fe0456d31e8c4977d1
Christian Weichsel <c.weichsel@gmx.de>
2021-06-20 08:47:23 +0200

    work1

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, Response
 app = Flask(__name__)
 
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.get("/")
 def upload_image():
+
     return render_template("upload.html")
+
+@app.post("/upload_files")
+def upload_files():
+    report = ""
+    for f in request.files.getlist("files[]"):
+        txt = f.read().decode("utf-8")
+        report += f.filename
+        report += txt
+        report += "\n"
+
+    print(report)
+    response = Response(report, mimetype="text/csv")
+    response.headers['Content-Disposition'] = 'attachment; filename=fehlerreport.txt'
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
